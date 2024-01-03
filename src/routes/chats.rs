5e29723bea857d54dd43a7427586b1e5fa68fce0
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-03 00:14:06 -0400

    feat: create chat

---
+++ src/routes/chats.rs
@@ -0,0 +1,32 @@
+use axum::{routing::post, Json, Router};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+pub fn chats_routes() -> Router {
+    Router::new().route("/create-chat", post(create_chat))
+}
+
+async fn create_chat(Json(chat_for_create): Json<ChatForCreate>) -> Result<Json<Chat>, String> {
+    let new_chat = Chat::new(chat_for_create);
+    Ok(Json(new_chat))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChatForCreate {
+    title: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Chat {
+    id: String,
+    title: String,
+}
+
+impl Chat {
+    fn new(chat: ChatForCreate) -> Self {
+        Chat {
+            id: Uuid::new_v4().to_string(),
+            title: chat.title,
+        }
+    }
+}
