ee4fe6ea7fd1975ac695c24dbeb849cd0587d73e
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-03 02:39:04 -0400

    feat: add cors (#7)
    * chore: install tower_http
    
    * feat: rename route
    
    * chore: install dotenv and .env.example and cors origins

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-use axum::Router;
+use std::env;
+
+use axum::{
+    http::{header, HeaderValue},
+    Router,
+};
+use dotenv::dotenv;
+use tower_http::cors::CorsLayer;
 
 mod routes;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest("/api", routes::routes());
+    dotenv().ok();
+    let origins_urls: Vec<HeaderValue> = env::var("CORS_ORIGINS")
+        .expect("CORS_ORIGINS must be set.")
+        .split(",")
+        .map(|url| url.parse::<HeaderValue>().unwrap())
+        .collect();
+
+    let app = Router::new().nest("/api", routes::routes()).layer(
+        CorsLayer::new()
+            .allow_origin(origins_urls)
+            .allow_headers([header::CONTENT_TYPE]),
+    );
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
+
     println!(
         "->> LISTENING ON http://{:?}\n",
         listener.local_addr().unwrap()
