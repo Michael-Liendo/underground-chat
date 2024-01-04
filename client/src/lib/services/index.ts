41e37d312b8d5527dbe23f53888e7c062839bb78
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-04 16:25:33 -0400

    chore: add to global service

--- client/src/lib/services/index.ts
+++ client/src/lib/services/index.ts
@@ -1,7 +1,9 @@
 import ChatService from './ChatService';
 import MessagesService from './MessagesService';
+import UserService from './UserService';
 
 export default class Service {
   static chat = ChatService;
   static message = MessagesService;
+  static user = UserService
 }
