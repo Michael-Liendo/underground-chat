73d78f12e30e62c3eea12473eba37c05eef57d5b
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-03 23:01:56 -0400

    feat: add socket.io config

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,50 @@ use axum::{
     Router,
 };
 use dotenv::dotenv;
+use serde_json::Value;
+use socketioxide::{
+    extract::{Bin, Data, SocketRef},
+    SocketIo,
+};
 use tower_http::cors::CorsLayer;
 
 mod routes;
 
+fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    // println!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
+
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            println!("Received event: {:?} {:?}", data, bin);
+            socket.bin(bin).emit("message-back", data).ok();
+        },
+    );
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
     let origins_urls: Vec<HeaderValue> = env::var("CORS_ORIGINS")
         .expect("CORS_ORIGINS must be set.")
         .split(",")
         .map(|url| url.parse::<HeaderValue>().unwrap())
         .collect();
 
-    let app = Router::new().nest("/api", routes::routes()).layer(
-        CorsLayer::new()
-            .allow_origin(origins_urls)
-            .allow_headers([header::CONTENT_TYPE]),
-    );
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/ws", on_connect);
+
+    let app = Router::new()
+        .nest("/api", routes::routes())
+        .layer(layer)
+        .layer(
+            CorsLayer::new()
+                .allow_origin(origins_urls)
+                .allow_headers([header::CONTENT_TYPE]),
+        );
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
