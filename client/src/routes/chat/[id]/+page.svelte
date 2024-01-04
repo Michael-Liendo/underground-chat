0f446eac152cdc3fe8426e8496c2acbf61340acb
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-03 23:02:44 -0400

    feat: socket io request

--- client/src/routes/chat/[id]/+page.svelte
+++ client/src/routes/chat/[id]/+page.svelte
@@ -1,6 +1,16 @@
-<script>
-  /** @type {import('./$types').PageData} */
-  export let data;
+<script lang="ts">
+  /** @type {import('./$types').RouteParams} */
+  import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  import { io } from 'socket.io-client';
+
+  onMount(() => {
+    const socket = io('http://127.0.0.1:3000/ws');
+
+    setTimeout(() => {
+      socket.emit('message', 'michael');
+    }, 5000);
+  });
 </script>
 
-<h1>{data.id}</h1>
+<h1>{$page.params.id}</h1>
