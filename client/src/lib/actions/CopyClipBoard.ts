a2c84ff75a3402f6f3857e8435dca18236592207
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-05 11:09:30 -0400

    chore: Fix import statement and update notification message

--- client/src/lib/actions/CopyClipBoard.ts
+++ client/src/lib/actions/CopyClipBoard.ts
@@ -1,9 +1,9 @@
-import { notifications } from "@whizzes/svelte-notifications";
+import { notifications } from '@whizzes/svelte-notifications';
 
 // copy to clipboard action svelte
 export const copyToClipboard = (text: string) => {
-  notifications.notifySuccess("Copied to clipboard");
-  
+  notifications.notifySuccess('Copied to clipboard');
+
   const el = document.createElement('textarea');
   el.value = text;
   document.body.appendChild(el);
