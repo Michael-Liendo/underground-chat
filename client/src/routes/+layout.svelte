c4b061f7a5d1b2826ae71db61a3133c1b5f7e543
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-05 11:09:07 -0400

    feat: notifications

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
+  import { NotificationList, Position } from '@whizzes/svelte-notifications';
+
   import '../app.css';
+  import { fade } from 'svelte/transition';
 </script>
 
 <slot />
+<NotificationList
+  class="fixed top-1 right-6"
+  position={Position.TopRight}
+  let:notification
+>
+  <div
+    in:fade
+    out:fade
+    class="bg-gray-100 rounded text-teal-900 px-4 py-3 shadow-md"
+    role="alert"
+  >
+    <div class="flex">
+      <div>
+        <p class="font-bold">{notification.message}</p>
+      </div>
+    </div>
+  </div>
+</NotificationList>
