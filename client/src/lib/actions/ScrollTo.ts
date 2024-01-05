34e2cf32d6e045fce3da266fb6fbe8d21de62589
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-04 23:40:11 -0400

    fix: lint issues

--- client/src/lib/actions/ScrollTo.ts
+++ client/src/lib/actions/ScrollTo.ts
@@ -1,5 +1,8 @@
 type Direction = 'up' | 'down';
 
-export default function ScrollTo(direction:Direction, element:HTMLElement){
-  element.scroll({behavior: 'smooth', top: direction === 'up' ? 0 : element.scrollHeight})
+export default function ScrollTo(direction: Direction, element: HTMLElement) {
+  element.scroll({
+    behavior: 'smooth',
+    top: direction === 'up' ? 0 : element.scrollHeight,
+  });
 }
