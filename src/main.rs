26dccf8154e9b49c4fecbaed620ce76a873dfc7e
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-12 12:29:34 -0400

    refactor: Add join and message event handlers

--- src/main.rs
+++ src/main.rs
@@ -4,57 +4,12 @@ use axum::{
     http::{header, HeaderValue},
     Router,
 };
-use chrono::{DateTime, Utc};
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
-use socketioxide::{
-    extract::{Data, SocketRef},
-    SocketIo,
-};
+use socketioxide::SocketIo;
 use tower_http::cors::CorsLayer;
 
+mod events;
 mod routes;
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Message {
-    content: String,
-    room: String,
-    username: String,
-    created_at: DateTime<Utc>,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Join {
-    room: String,
-    username: String,
-}
-
-fn on_connect(socket: SocketRef) {
-    socket.on("join", |socket: SocketRef, Data::<Join>(join)| async move {
-        socket.leave_all().ok();
-        socket.join(join.room.clone()).ok();
-
-        socket
-            .within(join.room.clone())
-            .broadcast()
-            .emit("join", join)
-            .ok();
-    });
-
-    socket.on(
-        "chat message",
-        |socket: SocketRef, Data::<Message>(data)| {
-            println!("Received event: {:?} \n", data);
-
-            let chat_id = &data.room;
-            socket
-                .within(chat_id.to_string())
-                .broadcast()
-                .emit("chat message", data)
-                .ok();
-        },
-    );
-}
 
 #[tokio::main]
 async fn main() {
@@ -71,7 +26,7 @@ async fn main() {
 
     let (layer, io) = SocketIo::new_layer();
 
-    io.ns("/ws", on_connect);
+    io.ns("/ws", events::on_connect);
 
     let app = Router::new()
         .nest("/api", routes::routes())
